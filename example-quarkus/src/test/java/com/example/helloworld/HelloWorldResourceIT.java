25c90a4d5f1164b24553052ac5c749e3d13141a6
r3adm3 <adrianjfarnell@gmail.com>
2025-09-30 16:12:44 +0100

    feat: added quarkus example app

---
+++ example-quarkus/src/test/java/com/example/helloworld/HelloWorldResourceIT.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class HelloWorldResourceIT extends HelloWorldResourceTest {
+    // Execute the same tests but in packaged mode.
+}
