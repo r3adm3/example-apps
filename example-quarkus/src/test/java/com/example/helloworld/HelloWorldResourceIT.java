be16f3e9c187fa53fc8ce0f5bf2cb90b1dd4a428
r3adm3 <adrianjfarnell@gmail.com>
2025-09-30 17:27:50 +0100

    feat: remove quarkus

--- example-quarkus/src/test/java/com/example/helloworld/HelloWorldResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.example.helloworld;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class HelloWorldResourceIT extends HelloWorldResourceTest {
-    // Execute the same tests but in packaged mode.
-}
